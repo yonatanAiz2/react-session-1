b44aadb0e847e5e79c35172fcae1fd1e230feb7f
yoniaiz <yoni29396@gmail.com>
2021-12-13 15:42:17 +0200

    connect query client

--- react-session-1-fe/src/index.tsx
+++ react-session-1-fe/src/index.tsx
@@ -5,14 +5,19 @@ import { PersistGate } from "redux-persist/integration/react";
 import store, { persistor } from "./store";
 import App from "./App";
 import { Spinner } from "./components/Spinner/Spinner";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const client = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={<Spinner />} persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <QueryClientProvider client={client}>
+      <Provider store={store}>
+        <PersistGate loading={<Spinner />} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
