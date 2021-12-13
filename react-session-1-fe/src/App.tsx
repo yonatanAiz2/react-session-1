794fb1a69720d19f671baed2cbaa147697682d92
yoniaiz <yoni29396@gmail.com>
2021-12-13 09:43:02 +0200

    connect lazy

--- react-session-1-fe/src/App.tsx
+++ react-session-1-fe/src/App.tsx
@@ -1,25 +1,30 @@
+import { lazy, Suspense } from "react";
 import Layout from "./components/Layout/Layout";
-import Themes from "./screens/Themes/Themes";
-import AddTheme from "./screens/AddTheme/AddTheme";
-import Login from "./screens/Login/Login";
-import Register from "./screens/Register/Register";
-import Profile from "./screens/Profile/Profile";
 import { GlobalStyle } from "./style/GlobalStyle.style";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { Spinner } from "./components/Spinner/Spinner";
+
+const Login = lazy(() => import("./screens/Login/Login"));
+const Register = lazy(() => import("./screens/Register/Register"));
+const Profile = lazy(() => import("./screens/Profile/Profile"));
+const Themes = lazy(() => import("./screens/Themes/Themes"));
+const AddTheme = lazy(() => import("./screens/AddTheme/AddTheme"));
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Layout>
-        <Switch>
-          <Route path="/" exact component={Themes} />
-          <Route path="/themes/:id" component={Themes} />
-          <Route path="/add-theme" component={AddTheme} />
-          <Route path="/login" component={Login} />
-          <Route path="/register" component={Register} />
-          <Route path="/profile" component={Profile} />
-        </Switch>
+        <Suspense fallback={<Spinner />}>
+          <Switch>
+            <Route path="/" exact component={Themes} />
+            <Route path="/themes/:id" component={Themes} />
+            <Route path="/add-theme" component={AddTheme} />
+            <Route path="/login" component={Login} />
+            <Route path="/register" component={Register} />
+            <Route path="/profile" component={Profile} />
+          </Switch>
+        </Suspense>
       </Layout>
     </BrowserRouter>
   );
