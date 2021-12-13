9601d2cc79682b1d8695de4fdfccd7406b3fe2cd
yoniaiz <yoni29396@gmail.com>
2021-12-13 11:12:30 +0200

    add theme connected

---
+++ react-session-1-fe/src/screens/AddTheme/AddTheme.style.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const ColorsContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin-top: 32px;
+`;
+
+export const InputContainer = styled.div`
+  margin: 16px 0;
+  width: 320px;
+  label {
+    margin: 14px;
+  }
+  input {
+    margin-top: 8px;
+  }
+`;
+
+export const Label = styled.label`
+  margin: 8px 0;
+`;
