d2f2795c4b0e45f255c36195cf7cb4b9b5ec225a
yoniaiz <yoni29396@gmail.com>
2021-12-13 08:43:24 +0200

    basic routing config

--- react-session-1-fe/src/App.tsx
+++ react-session-1-fe/src/App.tsx
@@ -1,15 +1,24 @@
 import Layout from "./components/Layout/Layout";
 import Themes from "./screens/Themes/Themes";
+import AddTheme from "./screens/AddTheme/AddTheme";
+import Login from "./screens/Login/Login";
+import Register from "./screens/Register/Register";
+import Profile from "./screens/Profile/Profile";
 import { GlobalStyle } from "./style/GlobalStyle.style";
+import { BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <GlobalStyle />
       <Layout>
         <Themes />
+        <AddTheme />
+        <Login />
+        <Register />
+        <Profile />
       </Layout>
-    </>
+    </BrowserRouter>
   );
 }
 
--- react-session-1-fe/src/screens/Themes/hooks/useFetchThemes.ts
+++ react-session-1-fe/src/screens/Themes/hooks/useFetchThemes.ts
@@ -3,18 +3,27 @@ import { useState, useEffect } from "react";
 import { themesMock } from "../../../utils/test-utils/mocks/themes";
 
 export const useFetchThemes = () => {
-  const status: StateStatus = "idle";
-  const themes: Theme[] = [];
+  const [status, setStatus] = useState<StateStatus>("idle");
+  const [themes, setThemes] = useState<Theme[]>([]);
 
   const getThemes = async () => {
+    setStatus("loading");
     try {
       const { data } = { data: themesMock }; // will be API call;
       const fetchedThemes: Theme[] = data.map((theme) => ({
         id: theme.id,
         ...theme.attributes,
       }));
-    } catch (e) {}
+
+      setThemes(fetchedThemes);
+      setStatus("success");
+    } catch (e) {
+      setStatus("rejected");
+    }
   };
+  useEffect(() => {
+    getThemes();
+  }, []);
 
   return { themes, status };
 };
